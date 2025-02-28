8afa442420fd7dfd716eafc897524021974683c5
rich.claussen <rclaussen@allmedmd.com>
2025-02-27 22:45:49 -0800

    Adding scripts for table creation and population

---
+++ dbScripts/PopulateCustomerTable.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers(FirstName, MiddleName, LastName, Email, Phone)
+VALUES
+	('Fred', null, 'Flintstone', 'fred@bedrock.com', '423'),
+	('Betty', 'J', 'Rubble', 'betty@bedrock.com', '549'),
+	('Wilma', 'Q', 'Flintstone', 'wilma@bedrock.com', '424'),
+	('Barney', null, 'Rubble', 'barney@bedrock.com', '548');
+
+SELECT * FROM Customers
